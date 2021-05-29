eae465ae727d572df0ef605d33e5d5533c4d4113
Diego Bermudez <godiegosaenz@gmail.com>
2021-05-29 10:39:04 -0500

    Instalacion de lavarel - datatables de yajrabox

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,6 +11,8 @@ try {
     window.$ = window.jQuery = require('jquery');
     require('bootstrap');
     require('admin-lte');
+    require('datatables.net-bs4');
+    require('datatables.net-buttons-bs4');
 } catch (e) {
 
 }
