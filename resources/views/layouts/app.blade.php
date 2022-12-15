82dc254af18fde96b4eb7823be3c291ffe0ff894
Diego Bermudez <godiegosaenz@gmail.com>
2022-12-15 12:44:34 -0500

    CAMBIO VISTA APP LAYOUT 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>Sistema de gestion de turismo</title>
-  <link rel="stylesheet" href="{{asse(mix('css/app.css'))}}">
+  <link rel="stylesheet" href="{{asset(mix('css/app.css'))}}">
 
   <!-- jQuery -->
    <script src="{{asset(mix('js/app.js'))}}"></script>
